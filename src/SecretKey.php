15bd39775be957068ca823d4796a33d26fb0ef6f
森太桜 <87011031+TaroMori0321@users.noreply.github.com>
2022-05-20 11:22:29 +0900

    Update SecretKey.php

--- src/SecretKey.php
+++ src/SecretKey.php
@@ -6,6 +6,14 @@ namespace Haikara\SymbolKeySodium;
 
 class SecretKey extends Key
 {
+    protected readonly string $key;
+    
+    private function __construct(string $key)
+    {
+        if (!static::is64Byte()) throw new \Exception();
+        $this->key = $key;
+    }
+    
     public function sign(string $message)
     {
         return sodium_crypto_sign($message, $this->key);
@@ -19,6 +27,17 @@ class SecretKey extends Key
         return new static($pk_str);
     }
 
+    /**
+     * 文字列が64バイトであるか
+     *
+     * @param string $string
+     * @return boolean
+     */
+    private static function is64byte(string $string): bool
+    {
+        return strlen($string) === 64;
+    }
+    
     /**
      * 文字列が96バイトであるか
      *
